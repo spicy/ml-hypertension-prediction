a993811958fa8c9549c874fa720465bc0ae845e7
spicycurrey <spiceycurrey@gmail.com>
2024-09-10 19:30:35 -0700

    update questions and data mappings

--- data_mappings.py
+++ data_mappings.py
@@ -1,14 +1,30 @@
 import json
-import pandas as pd
 
 with open('questions.json', 'r') as f:
     MAPPINGS = json.load(f)
+
+def map_value(value, mappings):
+    # Check for exact matches first
+    if str(value) in mappings:
+        return mappings[str(value)]
+
+    # Check for range matches
+    for key, mapped_value in mappings.items():
+        if '-' in key:
+            min_val, max_val = map(int, key.split('-'))
+            try:
+                if min_val <= int(value) <= max_val:
+                    return value if mapped_value == "Range of Values" else mapped_value
+            except ValueError:
+                pass  # If value can't be converted to int, skip this check
+
+    return 'Invalid Response'
 
 def apply_mappings(df):
     for category, questions in MAPPINGS.items():
         for question, data in questions.items():
             if question in df.columns:
-                df[f'{question}_description'] = df[question].astype(str).map(data['mappings'])
+                df[f'{question}_description'] = df[question].apply(lambda x: map_value(x, data['mappings']))
                 df[f'{question}_label'] = data['label']
     return df
 
