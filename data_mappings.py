92b6b04401c4b3eb004e6a5f81c534f3600a7c13
spicycurrey <spiceycurrey@gmail.com>
2024-09-20 18:14:07 -0700

    update questions formatting

--- data_mappings.py
+++ data_mappings.py
@@ -1,16 +1,28 @@
 import json
 
-with open('questions.json', 'r') as f:
+QUESTIONS_FILE = 'questions.json'
+NO_RESPONSE = 'No Response'
+DESCRIPTION_NOT_AVAILABLE = 'Description Not Available'
+INVALID_RESPONSE = 'Invalid Response'
+RANGE_OF_VALUES = "Range of Values"
+MAPPINGS_NOT_AVAILABLE = 'Mappings Not Available'
+LABEL_NOT_AVAILABLE = 'Label Not Available'
+DESCRIPTION_KEY = 'description'
+LABEL_KEY = 'label'
+MAPPINGS_KEY = 'mappings'
+POPULATE_KEY = 'populate'
+
+with open(QUESTIONS_FILE, 'r') as f:
     MAPPINGS = json.load(f)
 
 def map_value(value, mappings):
     # Check if value is None or empty
     if value is None or value == '':
-        return 'No Response'
+        return NO_RESPONSE
 
     # Check for exact matches first
     if str(value) in mappings:
-        return mappings[str(value)].get('description', 'Description Not Available')
+        return mappings[str(value)].get(DESCRIPTION_KEY, DESCRIPTION_NOT_AVAILABLE)
 
     # Check for range matches
     for key, mapped_value in mappings.items():
@@ -18,37 +30,37 @@ def map_value(value, mappings):
             try:
                 min_val, max_val = map(int, key.split('-'))
                 if min_val <= int(value) <= max_val:
-                    return value if mapped_value.get('description') == "Range of Values" else mapped_value.get('description', 'Description Not Available')
+                    return value if mapped_value.get(DESCRIPTION_KEY) == RANGE_OF_VALUES else mapped_value.get(DESCRIPTION_KEY, DESCRIPTION_NOT_AVAILABLE)
             except ValueError:
                 pass  # If value can't be converted to int, skip this check
 
-    return 'Invalid Response'
+    return INVALID_RESPONSE
 
 def apply_mappings(df):
     for category, questions in MAPPINGS.items():
         for question, data in questions.items():
             if question in df.columns:
-                if 'mappings' in data:
-                    df[f'{question}_description'] = df[question].apply(lambda x: map_value(x, data['mappings']))
+                if MAPPINGS_KEY in data:
+                    df[f'{question}_description'] = df[question].apply(lambda x: map_value(x, data[MAPPINGS_KEY]))
                 else:
-                    df[f'{question}_description'] = 'Mappings Not Available'
-                df[f'{question}_label'] = data.get('label', 'Label Not Available')
+                    df[f'{question}_description'] = MAPPINGS_NOT_AVAILABLE
+                df[f'{question}_label'] = data.get(LABEL_KEY, LABEL_NOT_AVAILABLE)
     return df
 
 def get_question_label(category, question):
     try:
-        return MAPPINGS[category][question]['label']
+        return MAPPINGS[category][question][LABEL_KEY]
     except KeyError:
-        return 'Label Not Available'
+        return LABEL_NOT_AVAILABLE
 
 def get_question_mappings(category, question):
     try:
-        return MAPPINGS[category][question]['mappings']
+        return MAPPINGS[category][question][MAPPINGS_KEY]
     except KeyError:
         return {}
 
 def get_question_populate(category, question, value):
     try:
-        return MAPPINGS[category][question]['mappings'][str(value)].get('populate', {})
+        return MAPPINGS[category][question][MAPPINGS_KEY][str(value)].get(POPULATE_KEY, {})
     except KeyError:
         return {}
